262cc87e9277d2719fc54e1ba5aa9804e3d6b549
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-09-09 14:10:11 +0900

    Feature/camera info (#5)
    why
    hard coded camera paremeters were used.
    what
    save used camera parameters as json file.
    use that json file with npy file.

---
+++ depthview/zed_camerainfo.py
@@ -0,0 +1,136 @@
+import inspect
+import pyzed.sl as sl
+from dataclasses import dataclass, field
+from dataclasses_json import dataclass_json
+from pathlib import Path
+
+def get_fx_fy_cx_cy(left_cam_params):
+    """
+    Note:
+        left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
+    """
+    return left_cam_params.fx, left_cam_params.fy, left_cam_params.cx, left_cam_params.cy
+
+
+def get_baseline(cam_info) -> float:
+    """
+    Note:
+        cam_info = zed.get_camera_information()
+    """
+    return cam_info.camera_configuration.calibration_parameters.get_camera_baseline()
+
+
+def load_settings():
+    from pathlib import Path
+    import toml
+    tomlname = sorted(Path("/usr/local/lib/zed/settings").glob("SN*.conf"))[0]
+    zed_settings = toml.load(tomlname)
+    print(zed_settings["STEREO"])
+
+@dataclass_json
+@dataclass
+class CameraParmeter:
+    fx: float = 0.0
+    fy: float = 0.0
+    cx: float = 0.0
+    cy: float = 0.0
+    baseline: float = 0.0
+
+    def get_current_setting(self, cam_info):
+        left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
+        self.fx, self.fy, self.cx, self.cy = get_fx_fy_cx_cy(left_cam_params)
+        self.baseline = get_baseline(cam_info)
+
+    def save_json(self, name: Path):
+        open(name, "wt").write(self.to_json())
+
+    @classmethod
+    def load_json(cls, name: Path):
+        return cls.from_json(open(name, "rt").read())
+
+    @classmethod
+    def create(cls, cam_info):
+        left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
+        fx, fy, cx, cy = get_fx_fy_cx_cy(left_cam_params)
+        baseline = get_baseline(cam_info)
+
+        return cls(fx=fx, fy=fy, cx=cx, cy=cy, baseline=baseline)
+
+if __name__ == "__main__":
+    # Create a ZED camera object
+    zed = sl.Camera()
+
+    # Set up initial parameters for the camera
+    init_params = sl.InitParameters()
+
+    # Open the camera
+    status = zed.open(init_params)
+    if status != sl.ERROR_CODE.SUCCESS:
+        print(f"Error opening camera: {status}")
+        exit(1)
+
+    # Retrieve camera information
+    cam_info = zed.get_camera_information()
+
+    for k, v in inspect.getmembers(cam_info):
+        print(k, v)
+
+    print(f"{cam_info.camera_configuration=}")
+    print(f"{cam_info.sensors_configuration=}")
+    left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
+    for k, v in inspect.getmembers(cam_info.camera_configuration):
+        print(k, v)
+
+    for k, v in inspect.getmembers(cam_info.sensors_configuration):
+        print(k, v)
+
+    for k, v in inspect.getmembers(cam_info.camera_configuration.calibration_parameters):
+        print(k, v)
+
+    # Access left and right camera parameters
+    left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
+    right_cam_params = cam_info.camera_configuration.calibration_parameters.right_cam
+
+    for k, v in inspect.getmembers(left_cam_params):
+        print(k, v)
+
+    for k, v in inspect.getmembers(right_cam_params):
+        print(k, v)
+
+    # Print some of the camera parameters
+    print("Left Camera Parameters:")
+    print(f"image_size: {left_cam_params.image_size.width} x {left_cam_params.image_size.height}")
+    print(f"Focal Length (fx, fy): {left_cam_params.fx}, {left_cam_params.fy}")
+    print(f"Principal Point (cx, cy): {left_cam_params.cx}, {left_cam_params.cy}")
+    print(f"Distortion Coefficients: {left_cam_params.disto}")
+
+    print("\nRight Camera Parameters:")
+    print(f"image_size: {right_cam_params.image_size.width} x {right_cam_params.image_size.height}")
+    print(f"Focal Length (fx, fy): {right_cam_params.fx}, {right_cam_params.fy}")
+    print(f"Principal Point (cx, cy): {right_cam_params.cx}, {right_cam_params.cy}")
+    print(f"Distortion Coefficients: {right_cam_params.disto}")
+    print("\n")
+    print(f"{cam_info.camera_configuration.calibration_parameters.get_camera_baseline()=}")
+
+    print(f"{get_fx_fy_cx_cy(left_cam_params)=}")
+    fx, fy, cx, cy = get_fx_fy_cx_cy(left_cam_params)
+    print(f"{get_baseline(cam_info)}")
+    baseline = get_baseline(cam_info)
+    # Close the camera
+    zed.close()
+
+    camera_parameter = CameraParmeter(fx=fx, fy=fy, cx=cx, cy=cy, baseline=baseline)
+    print(camera_parameter)
+    print(camera_parameter.to_json())
+
+    json_file = Path("tmp.json")
+    camera_parameter.save_json(json_file)
+    parameter = CameraParmeter.load_json(json_file)
+    print(f"{parameter=}")
+
+    camera_parameter2 = CameraParmeter()
+    camera_parameter2.get_current_setting(cam_info)
+    print(f"{camera_parameter2=}")
+
+    camera_parameter3 = CameraParmeter.create(cam_info)
+    print(f"{camera_parameter3=}")
