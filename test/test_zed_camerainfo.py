ed302855d7bc51f4cb3aaa2bae646b7dfaa54d5a
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-09-09 16:18:17 +0900

    add a test

--- test/test_zed_camerainfo.py
+++ test/test_zed_camerainfo.py
@@ -1,23 +1,18 @@
-import inspect
 import pyzed.sl as sl
 
-from depthview.zed_camerainfo import get_width_height_fx_fy_cx_cy, get_baseline
+from depthview.zed_camerainfo import get_width_height_fx_fy_cx_cy, get_baseline, CameraParmeter
 
 
 def test_get_baseline():
-    # Create a ZED camera object
     zed = sl.Camera()
 
-    # Set up initial parameters for the camera
     init_params = sl.InitParameters()
 
-    # Open the camera
     status = zed.open(init_params)
     if status != sl.ERROR_CODE.SUCCESS:
         print(f"Error opening camera: {status}")
         exit(1)
 
-    # Retrieve camera information
     cam_info = zed.get_camera_information()
     baseline = get_baseline(cam_info)
     assert 110 < baseline < 130
@@ -25,23 +20,16 @@ def test_get_baseline():
 
 
 def test_get_fx_fy_cx_cy():
-
-    # Create a ZED camera object
     zed = sl.Camera()
 
-    # Set up initial parameters forget_baseline(cam_info)()
-
     init_params = sl.InitParameters()
-    # Open the camera
     status = zed.open(init_params)
     if status != sl.ERROR_CODE.SUCCESS:
         print(f"Error opening camera: {status}")
         exit(1)
 
-    # Retrieve camera information
     cam_info = zed.get_camera_information()
 
-    # # Access left and right camera parameters
     left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
 
     width, height, fx, fy, cx, cy = get_width_height_fx_fy_cx_cy(left_cam_params)
@@ -58,4 +46,24 @@ def test_get_fx_fy_cx_cy():
     assert cx > 0
     assert cy > 0
 
+    zed.close()
+
+def test_camera_param_create():
+    zed = sl.Camera()
+
+    init_params = sl.InitParameters()
+    status = zed.open(init_params)
+    if status != sl.ERROR_CODE.SUCCESS:
+        print(f"Error opening camera: {status}")
+        exit(1)
+
+    cam_info = zed.get_camera_information()
+    camera_parameter = CameraParmeter.create(cam_info)
+    print(f"{camera_parameter=}")
+    assert isinstance(camera_parameter.width, int)
+    assert isinstance(camera_parameter.height, int)
+    assert isinstance(camera_parameter.fx, float)
+    assert isinstance(camera_parameter.fy, float)
+    assert isinstance(camera_parameter.cx, float)
+    assert isinstance(camera_parameter.cy, float)
     zed.close()
