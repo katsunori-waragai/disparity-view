1ac7955bf3db85983198c85b17d091e97af9b3e0
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-10 16:03:19 +0900

    moved to module

--- depth_to_normal_map.py
+++ depth_to_normal_map.py
@@ -1,51 +1,6 @@
 import cv2
-import numpy as np
-from dataclasses import dataclass, field
 
-
-@dataclass
-class DepthToNormalMap:
-    """A class for converting a depth map image to a normal map image.
-
-
-    Attributes:
-        depth_map (ndarray): A numpy array representing the depth map image.
-        max_depth (int): The maximum depth value in the depth map image.
-    """
-
-    depth_map: np.ndarray = field(default=None)
-    max_depth: int = 255
-
-    def convert(self, depth_map: np.ndarray) -> np.ndarray:
-        """Converts the depth map image to a normal map image.
-
-        Args:
-            output_path (str): The path to save the normal map image file.
-
-        """
-        self.depth_map = depth_map
-        rows, cols = self.depth_map.shape[:2]
-
-        x, y = np.meshgrid(np.arange(cols), np.arange(rows))
-        x = x.astype(np.float32)
-        y = y.astype(np.float32)
-
-        # Calculate the partial derivatives of depth with respect to x and y
-        dx = cv2.Sobel(self.depth_map, cv2.CV_32F, 1, 0)
-        dy = cv2.Sobel(self.depth_map, cv2.CV_32F, 0, 1)
-
-        # Compute the normal vector for each pixel
-        normal = np.dstack((-dx, -dy, np.ones((rows, cols))))
-        norm = np.sqrt(np.sum(normal**2, axis=2, keepdims=True))
-        normal = np.divide(normal, norm, out=np.zeros_like(normal), where=norm != 0)
-
-        # Map the normal vectors to the [0, 255] range and convert to uint8
-        normal = (normal + 1) * 127.5
-        normal = normal.clip(0, 255).astype(np.uint8)
-
-        # Save the normal map to a file
-        return cv2.cvtColor(normal, cv2.COLOR_RGB2BGR)
-
+import disparity_view
 
 if __name__ == "__main__":
     import argparse
@@ -61,7 +16,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    converter = DepthToNormalMap(max_depth=args.max_depth)
+    converter = disparity_view.DepthToNormalMap(max_depth=args.max_depth)
     depth_map = cv2.imread(args.input, cv2.IMREAD_UNCHANGED)
     normal_bgr = converter.convert(depth_map)
     cv2.imwrite(args.output_path, normal_bgr)
---
+++ disparity_view/depth_to_normal.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass, field
+
+import cv2
+import numpy as np
+
+
+@dataclass
+class DepthToNormalMap:
+    """A class for converting a depth map image to a normal map image.
+
+
+    Attributes:
+        depth_map (ndarray): A numpy array representing the depth map image.
+        max_depth (int): The maximum depth value in the depth map image.
+    """
+
+    depth_map: np.ndarray = field(default=None)
+    max_depth: int = 255
+
+    def convert(self, depth_map: np.ndarray) -> np.ndarray:
+        """Converts the depth map image to a normal map image.
+
+        Args:
+            output_path (str): The path to save the normal map image file.
+
+        """
+        self.depth_map = depth_map
+        rows, cols = self.depth_map.shape[:2]
+
+        x, y = np.meshgrid(np.arange(cols), np.arange(rows))
+        x = x.astype(np.float32)
+        y = y.astype(np.float32)
+
+        # Calculate the partial derivatives of depth with respect to x and y
+        dx = cv2.Sobel(self.depth_map, cv2.CV_32F, 1, 0)
+        dy = cv2.Sobel(self.depth_map, cv2.CV_32F, 0, 1)
+
+        # Compute the normal vector for each pixel
+        normal = np.dstack((-dx, -dy, np.ones((rows, cols))))
+        norm = np.sqrt(np.sum(normal**2, axis=2, keepdims=True))
+        normal = np.divide(normal, norm, out=np.zeros_like(normal), where=norm != 0)
+
+        # Map the normal vectors to the [0, 255] range and convert to uint8
+        normal = (normal + 1) * 127.5
+        normal = normal.clip(0, 255).astype(np.uint8)
+
+        return cv2.cvtColor(normal, cv2.COLOR_RGB2BGR)
