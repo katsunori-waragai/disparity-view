aae030b80b80a18ee52323f1e85558bb04e39f9c
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-10-11 16:38:15 +0900

    under development

--- scripts/view_npy.py
+++ scripts/view_npy.py
@@ -1,10 +1,12 @@
 import argparse
 from pathlib import Path
 
+import cv2
 import numpy as np
 from tqdm import tqdm
 
 from disparity_view.view import view_npy
+import disparity_view
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="np file viewer")
@@ -17,6 +19,7 @@ if __name__ == "__main__":
     group.add_argument("--gray", action="store_true", help="gray colormap")
     group.add_argument("--jet", action="store_true", help="jet colormap")
     group.add_argument("--inferno", action="store_true", help="inferno colormap")
+    group.add_argument("--normal", action="store_true", help="normal colormap")
 
     args = parser.parse_args()
     print(args)
@@ -27,6 +30,16 @@ if __name__ == "__main__":
         npys = sorted(Path(args.npy_file).glob("*.npy"))
         for npy in tqdm(npys):
             disparity = np.load(npy)
-            view_npy(disparity, args)
+            if args.normal:
+                print(f"{args.normal=}")
+                converter = disparity_view.DepthToNormalMap()
+                depth_map = 1.0 / disparity
+                normal_bgr = converter.convert(depth_map)
+                print(f"{normal_bgr.shape=}")
+                oname = Path("output") / f"normal_{npy.stem}.png"
+                oname.parent.mkdir(exist_ok=True)
+                cv2.imwrite(str(oname), normal_bgr)
+            else:
+                view_npy(disparity, args)
     else:
         print(f"no such file {args.npy_file}")
