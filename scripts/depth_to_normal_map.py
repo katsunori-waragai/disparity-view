32e0ce8594a4f8d7dec950c9f50a06207bffe72b
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-10-11 16:50:18 +0900

    simplified using cv2.IMREAD_GRAYSCALE)

--- scripts/depth_to_normal_map.py
+++ scripts/depth_to_normal_map.py
@@ -16,9 +16,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     converter = disparity_view.DepthToNormalMap()
-    depth_map = cv2.imread(args.input, cv2.IMREAD_UNCHANGED)
-    if len(depth_map.shape) == 3:
-        depth_map = cv2.cvtColor(depth_map, cv2.COLOR_BGR2GRAY)
+    depth_map = cv2.imread(args.input, cv2.IMREAD_GRAYSCALE)
     normal_bgr = converter.convert(depth_map)
     cv2.imwrite(args.output_path, normal_bgr)
     print(f"saved {args.output_path}")
