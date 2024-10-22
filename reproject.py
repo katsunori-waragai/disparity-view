073c21139745a676c891b932d0d286dfee98d810
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-10-22 18:25:58 +0900

    Feature/reproject less open3d (#52)
    why
    算出済みの点群の品質を確認したい。
    what
    点群の算出後に、右カメラ画像を再投影する関数をモジュールに追加した。

---
+++ reproject.py
@@ -0,0 +1,31 @@
+"""
+導出済みの視差画像に基づいて、右カメラでの再投影画像を生成するサンプルスクリプト
+"""
+
+import numpy as np
+import cv2
+
+from disparity_view.reprojection import reproject_from_left_and_disparity
+
+
+def dummy_camera_matrix(image_shape) -> np.ndarray:
+    # 近似値
+    cx = image_shape[1] / 2.0
+    cy = image_shape[0] / 2.0
+
+    # ダミー
+    fx = 1070  # [mm]
+    fy = fx
+
+    # カメラパラメータの設定
+    camera_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+    return camera_matrix
+
+
+if __name__ == "__main__":
+    imfile1 = "test/test-imgs/left/left_motorcycle.png"
+    left_image = cv2.imread(str(imfile1))
+    camera_matrix = dummy_camera_matrix(left_image.shape)
+    disparity = np.load("test/test-imgs/disparity-IGEV/left_motorcycle.npy")
+    reprojected_image = reproject_from_left_and_disparity(left_image, disparity, camera_matrix)
+    cv2.imwrite("reprojected.png", reprojected_image)
