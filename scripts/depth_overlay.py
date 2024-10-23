b257726de28c42d01de9f9c46f718d264f57fc97
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-23 10:34:24 +0900

    simplified scripts/depth_overlay.py

--- scripts/depth_overlay.py
+++ scripts/depth_overlay.py
@@ -28,22 +28,10 @@ if __name__ == "__main__":
     disparity = np.load(str(disparity_name))
     gray = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY)
 
-    if args.jet:
-        color_depth = disparity_view.as_colorimage(disparity, vmax=None, vmin=None, colormap=cv2.COLORMAP_JET)
-    elif args.inferno:
-        color_depth = disparity_view.as_colorimage(disparity, vmax=None, vmin=None, colormap=cv2.COLORMAP_INFERNO)
-    else:
-        color_depth = disparity_view.as_colorimage(disparity, vmax=None, vmin=None, colormap=cv2.COLORMAP_JET)
-
+    colormap = cv2.COLORMAP_INFERNO if args.inferno else cv2.COLORMAP_JET
+    color_depth = disparity_view.as_colorimage(disparity, vmax=None, vmin=None, colormap=colormap)
 
     overlayed = disparity_view.depth_overlay(gray, color_depth)
-
-    assert len(overlayed.shape) == 3
-    assert overlayed.shape[2] == 3
-    assert overlayed.shape[:2] == color_depth.shape[:2]
-
     outname = outdir / f"overlay_{left_name.stem}.png"
     outname.parent.mkdir(exist_ok=True, parents=True)
-
     cv2.imwrite(str(outname), overlayed)
-
