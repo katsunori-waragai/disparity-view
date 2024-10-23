b2c2afed8a52b5bed3bea2c0e7dc41931886bb5b
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-23 10:26:08 +0900

    added as_colorimage

--- disparity_view/__init__.py
+++ disparity_view/__init__.py
@@ -1,5 +1,5 @@
 from .view import view_npy
-from .view import depth_overlay
+from .view import depth_overlay, as_colorimage
 from .depth_to_normal import DepthToNormalMap
 from .zed_info import (
     get_width_height_fx_fy_cx_cy,
