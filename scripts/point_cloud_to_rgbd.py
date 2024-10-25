51764a4a185b4f158daab7ebda4a0dbff18a2149
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-25 19:02:27 +0900

    simplified

--- scripts/point_cloud_to_rgbd.py
+++ scripts/point_cloud_to_rgbd.py
@@ -7,31 +7,19 @@
 
 import open3d as o3d
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 import skimage.io
 
-if __name__ == '__main__':
-    device = o3d.core.Device('CPU:0')
+if __name__ == "__main__":
+    device = o3d.core.Device("CPU:0")
     tum_data = o3d.data.SampleTUMRGBDImage()
     depth = o3d.t.io.read_image(tum_data.depth_path).to(device)
     color = o3d.t.io.read_image(tum_data.color_path).to(device)
 
-    intrinsic = o3d.core.Tensor([[535.4, 0, 320.1], [0, 539.2, 247.6],
-                                 [0, 0, 1]])
+    intrinsic = o3d.core.Tensor([[535.4, 0, 320.1], [0, 539.2, 247.6], [0, 0, 1]])
     rgbd = o3d.t.geometry.RGBDImage(color, depth)
 
-    pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(rgbd,
-                                                           intrinsic,
-                                                           depth_scale=5000.0,
-                                                           depth_max=10.0)
-    # o3d.visualization.draw([pcd])
-    rgbd_reproj = pcd.project_to_rgbd_image(640,
-                                            480,
-                                            intrinsic,
-                                            depth_scale=5000.0,
-                                            depth_max=10.0)
+    pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic, depth_scale=5000.0, depth_max=10.0)
+    rgbd_reproj = pcd.project_to_rgbd_image(640, 480, intrinsic, depth_scale=5000.0, depth_max=10.0)
 
-    fig, axs = plt.subplots(1, 2)
     skimage.io.imsave("color.png", np.asarray(rgbd_reproj.color.to_legacy()))
     skimage.io.imsave("depth.png", np.asarray(rgbd_reproj.depth.to_legacy()))
