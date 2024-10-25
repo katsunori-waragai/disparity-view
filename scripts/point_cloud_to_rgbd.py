2f82846cebc14a3b4573865361565fbc411d1881
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-25 22:40:38 +0900

    reduced magic number

--- scripts/point_cloud_to_rgbd.py
+++ scripts/point_cloud_to_rgbd.py
@@ -12,11 +12,18 @@ if __name__ == "__main__":
     depth = o3d.t.io.read_image(tum_data.depth_path).to(device)
     color = o3d.t.io.read_image(tum_data.color_path).to(device)
 
+    assert depth.rows == color.rows
+    assert depth.columns == color.columns
+    print(f"{color.rows=} {color.columns=}")
+
+    width = color.columns
+    height = color.rows
+
     intrinsic = o3d.core.Tensor([[535.4, 0, 320.1], [0, 539.2, 247.6], [0, 0, 1]])
     rgbd = o3d.t.geometry.RGBDImage(color, depth)
 
     pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic, depth_scale=5000.0, depth_max=10.0)
-    rgbd_reproj = pcd.project_to_rgbd_image(640, 480, intrinsic, depth_scale=5000.0, depth_max=10.0)
+    rgbd_reproj = pcd.project_to_rgbd_image(width, height, intrinsic, depth_scale=5000.0, depth_max=10.0)
 
     color_legacy = np.asarray(rgbd_reproj.color.to_legacy())
     depth_legacy = np.asarray(rgbd_reproj.depth.to_legacy())
