ffe0224ae80e8327b1deebd140c871672856806a
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-10-25 19:31:16 +0900

    Feature/no intractive (#71)
    why
    サンプルスクリプトではGUIで入力待ちになってしまう。
    ファイルへの保存がない。
    what
    GUIをなくした。
    skimage.io.imsave() を利用した。

--- scripts/point_cloud_to_rgbd.py
+++ scripts/point_cloud_to_rgbd.py
@@ -1,36 +1,31 @@
-# ----------------------------------------------------------------------------
-# -                        Open3D: www.open3d.org                            -
-# ----------------------------------------------------------------------------
-# Copyright (c) 2018-2024 www.open3d.org
-# SPDX-License-Identifier: MIT
-# ----------------------------------------------------------------------------
-
 import open3d as o3d
 import numpy as np
-import matplotlib.pyplot as plt
+import skimage.io
+import cv2
 
-if __name__ == '__main__':
-    device = o3d.core.Device('CPU:0')
+if __name__ == "__main__":
+    """
+    pcd.project_to_rgbd_imageの使い方を確認するためのスクリプト
+    """
+    device = o3d.core.Device("CPU:0")
     tum_data = o3d.data.SampleTUMRGBDImage()
     depth = o3d.t.io.read_image(tum_data.depth_path).to(device)
     color = o3d.t.io.read_image(tum_data.color_path).to(device)
 
-    intrinsic = o3d.core.Tensor([[535.4, 0, 320.1], [0, 539.2, 247.6],
-                                 [0, 0, 1]])
+    intrinsic = o3d.core.Tensor([[535.4, 0, 320.1], [0, 539.2, 247.6], [0, 0, 1]])
     rgbd = o3d.t.geometry.RGBDImage(color, depth)
 
-    pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(rgbd,
-                                                           intrinsic,
-                                                           depth_scale=5000.0,
-                                                           depth_max=10.0)
-    o3d.visualization.draw([pcd])
-    rgbd_reproj = pcd.project_to_rgbd_image(640,
-                                            480,
-                                            intrinsic,
-                                            depth_scale=5000.0,
-                                            depth_max=10.0)
+    pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic, depth_scale=5000.0, depth_max=10.0)
+    rgbd_reproj = pcd.project_to_rgbd_image(640, 480, intrinsic, depth_scale=5000.0, depth_max=10.0)
 
-    fig, axs = plt.subplots(1, 2)
-    axs[0].imshow(np.asarray(rgbd_reproj.color.to_legacy()))
-    axs[1].imshow(np.asarray(rgbd_reproj.depth.to_legacy()))
-    plt.show()
+    color_legacy = np.asarray(rgbd_reproj.color.to_legacy())
+    depth_legacy = np.asarray(rgbd_reproj.depth.to_legacy())
+    print(f"{color_legacy.dtype=}")
+    print(f"{depth_legacy.dtype=}")
+    skimage.io.imsave("color.png", color_legacy)
+    skimage.io.imsave("depth.png", depth_legacy)
+
+    color_img = skimage.img_as_ubyte(color_legacy)
+    cv2.imwrite("color_ubyte.png", color_img[:, :, ::-1])
+    # depth_img = skimage.img_as_ubyte(depth_legacy)
+    # cv2.imwrite("depth_ubyte.png", depth_img)
