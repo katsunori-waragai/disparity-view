575b04e3c65800f077c17f05f9436d09335e06cb
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-25 19:56:39 +0900

    still un der develop

--- scripts/reproject_open3d.py
+++ scripts/reproject_open3d.py
@@ -63,20 +63,16 @@ def reproject_point_cloud(
     device = o3d.core.Device("CPU:0")
     pcd.transform([[1, 0, 0, baseline], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
-    # 投影行列の作成
-
     open3d_right_intrinsic = right_camera_intrinsics
 
     print(f"{open3d_right_intrinsic=}")
 
-    rgbd_reproj = pcd.project_to_rgbd_image(640, 480, intrinsic, depth_scale=5000.0, depth_max=10.0)
-
+    shape = [left_image.rows, left_image.columns]
+    rgbd_reproj = pcd.project_to_rgbd_image(shape[1], shape[0], intrinsic, depth_scale=5000.0, depth_max=10.0)
     color_legacy = np.asarray(rgbd_reproj.color.to_legacy())
     depth_legacy = np.asarray(rgbd_reproj.depth.to_legacy())
     print(f"{color_legacy.dtype=}")
     print(f"{depth_legacy.dtype=}")
-    # skimage.io.imsave("color.png", color_legacy)
-    # skimage.io.imsave("depth.png", depth_legacy)
     color_img = skimage.img_as_ubyte(color_legacy)
 
     return color_img
@@ -89,15 +85,16 @@ if __name__ == "__main__":
 
     device = o3d.core.Device("CPU:0")
     imfile1 = "../test/test-imgs/left/left_motorcycle.png"
-    left_image = o3d.t.io.read_image(str(imfile1))
+    left_image = o3d.t.io.read_image(str(imfile1)).to(device)
 
-    for k, v in inspect.getmembers(left_image):
-        print(k, v)
+    if 0:
+        for k, v in inspect.getmembers(left_image):
+            print(k, v)
 
     # disparity = o3d.geometry.Image(np.load("../test/test-imgs/disparity-IGEV/left_motorcycle.npy"))
     disparity = np.load("../test/test-imgs/disparity-IGEV/left_motorcycle.npy")
 
-    shape = [left_image.rows, left_image.rows]
+    shape = [left_image.rows, left_image.columns]
     intrinsic = dummy_pihhole_camera_intrincic(shape)
     # 基線長の設定
     baseline = 120  # カメラ間の距離[m]
