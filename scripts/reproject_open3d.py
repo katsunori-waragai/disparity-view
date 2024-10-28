0b0cbd095fdcfb07895e8d7778be74e714ec7393
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-28 18:35:11 +0900

    make flat

--- scripts/reproject_open3d.py
+++ scripts/reproject_open3d.py
@@ -8,74 +8,11 @@ import inspect
 from disparity_view.util import dummy_pihhole_camera_intrincic
 
 
-def o3d_generate_point_cloud(
-    disparity_map, left_image, intrinsic: o3d.camera.PinholeCameraIntrinsic, baseline: float
-) -> o3d.geometry.PointCloud:
-    """
-    視差マップと左カメラのRGB画像から点群データを生成する
-
-    Args:
-        disparity_map: 視差マップ (HxW)
-        left_image: 左カメラのRGB画像 (HxWx3)
-        camera_matrix: カメラの内部パラメータ
-        baseline: 基線長
-
-    Returns:
-        pcd: Open3DのPointCloudオブジェクト
-    """
-
-    # 視差から深度を計算
-    focal_length, _ = intrinsic.get_focal_length()
-    depth = baseline * focal_length / (disparity_map + 1e-8)
-
-    open3d_img = o3d.geometry.Image(left_image)
-    open3d_depth = o3d.geometry.Image(depth)
-    # 深度マップとカラー画像から点群を作成
-    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(open3d_img, open3d_depth)
-
-    def shape(left_image):
-        if isinstance(left_image, np.ndarray):
-            return left_image.shape
-        else:
-            return (left_image.rows, left_image.columns)
-
-    intrinsic = dummy_pihhole_camera_intrincic(shape(left_image))
-    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic=intrinsic)
-    return pcd
-
-
-def reproject_point_cloud(
-    pcd: o3d.geometry.PointCloud, right_camera_intrinsics: o3d.camera.PinholeCameraIntrinsic, baseline: float
-):
-    """
-    点群データを右カメラ視点に再投影する
-
-    Args:
-        pcd: Open3DのPointCloudオブジェクト
-        right_camera_intrinsics: 右カメラの内部パラメータ
-        baseline: 基線長
-
-    Returns:
-        reprojected_image: 再投影画像
-    """
-
-    # 視点変換（平行移動）
-    device = o3d.core.Device("CPU:0")
-    pcd.transform([[1, 0, 0, baseline], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-
-    open3d_right_intrinsic = right_camera_intrinsics
-
-    print(f"{open3d_right_intrinsic=}")
-
-    shape = [left_image.rows, left_image.columns]
-    rgbd_reproj = pcd.project_to_rgbd_image(shape[1], shape[0], intrinsic, depth_scale=5000.0, depth_max=10.0)
-    color_legacy = np.asarray(rgbd_reproj.color.to_legacy())
-    depth_legacy = np.asarray(rgbd_reproj.depth.to_legacy())
-    print(f"{color_legacy.dtype=}")
-    print(f"{depth_legacy.dtype=}")
-    color_img = skimage.img_as_ubyte(color_legacy)
-
-    return color_img
+def shape(left_image):
+    if isinstance(left_image, np.ndarray):
+        return left_image.shape
+    else:
+        return (left_image.rows, left_image.columns)
 
 
 if __name__ == "__main__":
@@ -133,6 +70,20 @@ if __name__ == "__main__":
     assert isinstance(pcd, o3d.geometry.PointCloud)
 
     # 再投影
-    reprojected_image = reproject_point_cloud(pcd, right_camera_intrinsics, baseline)
+    device = o3d.core.Device("CPU:0")
+    pcd.transform([[1, 0, 0, baseline], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+    open3d_right_intrinsic = right_camera_intrinsics
+
+    print(f"{open3d_right_intrinsic=}")
+
+    shape = [left_image.rows, left_image.columns]
+    rgbd_reproj = pcd.project_to_rgbd_image(shape[1], shape[0], intrinsic, depth_scale=5000.0, depth_max=10.0)
+    color_legacy = np.asarray(rgbd_reproj.color.to_legacy())
+    depth_legacy = np.asarray(rgbd_reproj.depth.to_legacy())
+    print(f"{color_legacy.dtype=}")
+    print(f"{depth_legacy.dtype=}")
+    reprojected_image = skimage.img_as_ubyte(color_legacy)
+
     if isinstance(reprojected_image, np.ndarray):
         cv2.imwrite("reprojected_open3d.png", reprojected_image)
