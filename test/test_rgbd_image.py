ac837059f26b24873953e6b0a93f8652d298cfb6
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-10-29 17:09:00 +0900

    Feature/less inspect (#85)
    what
    test/test_*.py 中のinspect の利用を削除した。

--- test/test_rgbd_image.py
+++ test/test_rgbd_image.py
@@ -1,11 +1,11 @@
 import open3d as o3d
 import numpy as np
 
-import inspect
 
 tum_data = o3d.data.SampleTUMRGBDImage()
 depth_path = tum_data.depth_path
 color_path = tum_data.color_path
+
 
 def test_t_rgbd_image():
     device = o3d.core.Device("CPU:0")
@@ -34,14 +34,8 @@ def test_t_create_from_rgbd_image():
     intrinsic = o3d.core.Tensor([[535.4, 0, 320.1], [0, 539.2, 247.6], [0, 0, 1]])
     rgbd = o3d.t.geometry.RGBDImage(color, depth)
 
-
     pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic, depth_scale=5000.0, depth_max=10.0)
     assert hasattr(pcd, "project_to_rgbd_image")
-
-    # このようにすると、pcdがどのようなメソッドを持っているのかが、わかる。
-    for k, v in inspect.getmembers(pcd):
-        if str(v).find("method") > -1:
-            print(f"{k=} {v=}")
 
     rgbd_reproj = pcd.project_to_rgbd_image(width, height, intrinsic, depth_scale=5000.0, depth_max=10.0)
 
@@ -69,6 +63,7 @@ def test_rgbd_image():
 
     assert hasattr(rgbd, "color")
     assert hasattr(rgbd, "depth")
+
 
 if __name__ == "__main__":
     test_t_rgbd_image()
