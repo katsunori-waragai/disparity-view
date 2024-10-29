8005f0b73f0ce58fcf4448901614d173ccf4eeb5
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-10-29 15:12:20 +0900

    Feature/more point cloud (#80)
    why
    o3d.t.geometry.RGBDImage() のように、".t"のついているものとないものとの差がわかりにくい。
    そのため、使い方を確認すること。
    what
    以下のファイルに関数を追加。
    test/test_point_cloud.py

--- test/test_point_cloud.py
+++ test/test_point_cloud.py
@@ -1,8 +1,11 @@
+import inspect
 from pathlib import Path
 
 import open3d as o3d
 import numpy as np
 import skimage.io
+
+from disparity_view.util import dummy_pinhole_camera_intrincic
 
 tum_data = o3d.data.SampleTUMRGBDImage()
 depth_path = tum_data.depth_path
@@ -11,7 +14,7 @@ color_path = tum_data.color_path
 
 def test_t_point_cloud():
     """
-    read depth, color from files, and reproject from constructed point cloud
+    o3d.t を利用するデータ構造ベースでのpoint_cloud の使い方を確認するためのコード
     """
     print(f"{depth_path=}")
     print(f"{color_path=}")
@@ -49,9 +52,43 @@ def test_t_point_cloud():
     assert depth_legacy.dtype == np.float32
 
 
-if __name__ == "__main__":
+def test_point_cloud():
     """
-    pcd.project_to_rgbd_imageの使い方を確認するためのスクリプト
+    o3d.t を利用するデータ構造ベースでのpoint_cloud の使い方を確認するためのコード
+    """
+    depth = o3d.io.read_image(depth_path)
+    color = o3d.io.read_image(color_path)
+
+    height, width = np.array(color).shape[:2]
+
+    intrinsic = o3d.core.Tensor([[535.4, 0, 320.1], [0, 539.2, 247.6], [0, 0, 1]])
+    """
+    wrong code:
+    No such constructor
+    o3d.geometry.RGBDImage(color, depth)
+    """
+    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color, depth)
+
+    assert isinstance(rgbd, o3d.geometry.RGBDImage)
+    intrinsic = dummy_pinhole_camera_intrincic((height, width))
+
+    assert isinstance(intrinsic, o3d.cpu.pybind.camera.PinholeCameraIntrinsic)
+
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic)
+    if 0:
+        for k, v in inspect.getmembers(pcd):
+            if str(v).find("method") > -1:
+                print(f"{k=} {v=}")
+
+    assert hasattr(pcd, "project_to_rgbd_image") == False
+
+    """
+    o3d.geometry.PointCloud 型に再投影するためのメソッドは見つからなかった。
     """
 
+
+if __name__ == "__main__":
+
     test_t_point_cloud()
+
+    test_point_cloud()
