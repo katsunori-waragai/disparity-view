c8cc78e769e0838efc3d07cda60a20000b62eae9
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-30 16:42:22 +0900

    added prefix cv_

--- disparity_view/__init__.py
+++ disparity_view/__init__.py
@@ -1,8 +1,8 @@
 from .view import view_npy
 from .view import depth_overlay, as_colorimage
 from .depth_to_normal import DepthToNormalMap
-from .cv_reprojection import reproject_from_left_and_disparity
-from .cv_reprojection import gen_right_image, make_animation_gif
+from .cv_reprojection import cv_reproject_from_left_and_disparity, cv_gen_right_image
+from .cv_reprojection import make_animation_gif
 
 from .zed_info import (
     get_width_height_fx_fy_cx_cy,
