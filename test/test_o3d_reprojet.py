bf2dc891ff43041b49a51b2463504badd02ce327
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-10-30 13:48:40 +0900

    add test_o3d_reprojet.py (#89)
    why
    open3d で書き直したい
    what
    関数の抽出
    scripts/reproject_open3d.py
    以下のファイルを追加
    test/test_o3d_reprojet.py

--- test/test_o3d_reprojet.py
+++ test/test_o3d_reprojet.py
@@ -27,18 +27,20 @@ def shape_of(image) -> Tuple[float, float]:
     else:
         return (image.rows, image.columns)
 
-
-def disparity_to_depth(disparity: np.ndarray, baseline: float, focal_length: float) -> np.ndarray:
+def disparity_to_depth(disparity: np.ndarray, baseline: float, focal_length: float):
     depth = baseline * focal_length / (disparity + 1e-8)
     return depth
 
+def test_o3d_reproject():
+    from pathlib import Path
 
-def o3d_gen_right_image(disparity: np.ndarray, left_image: np.ndarray, outdir, left_name, axis):
+    device = o3d.core.Device("CPU:0")
+    imfile1 = "../test/test-imgs/left/left_motorcycle.png"
+    left_image = o3d.t.io.read_image(str(imfile1)).to(device)
 
-    DEPTH_SCALE = 1000.0
-    DEPTH_MAX = 10.0
-    left_name = Path(left_name)
-    shape = left_image.shape
+    disparity = np.load("../test/test-imgs/disparity-IGEV/left_motorcycle.npy")
+
+    shape = (left_image.rows, left_image.columns)
 
     # disparityからdepth にする関数を抜き出すこと
     intrinsic = o3d.core.Tensor([[535.4, 0, 320.1], [0, 539.2, 247.6], [0, 0, 1]])
@@ -50,54 +52,60 @@ def o3d_gen_right_image(disparity: np.ndarray, left_image: np.ndarray, outdir, l
     focal_length = 535.4
 
     depth = disparity_to_depth(disparity, baseline, focal_length)
+
+    print(f"{np.max(depth.flatten())=}")
+
     depth = np.array(depth, dtype=np.uint16)
 
     open3d_img = o3d.t.geometry.Image(left_image)
     open3d_depth = o3d.t.geometry.Image(depth)
+
+    o3d.t.io.write_image("depth_my.png", open3d_depth)
 
     rgbd = o3d.t.geometry.RGBDImage(open3d_img, open3d_depth)
 
     assert isinstance(rgbd, o3d.t.geometry.RGBDImage)
     assert isinstance(intrinsic, o3d.cpu.pybind.core.Tensor)
     pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(
-        rgbd, intrinsics=intrinsic, depth_scale=DEPTH_SCALE, depth_max=DEPTH_MAX
+        rgbd, intrinsics=intrinsic, depth_scale=1000.0, depth_max=10.0
     )
 
     assert isinstance(pcd, o3d.geometry.PointCloud) or isinstance(pcd, o3d.t.geometry.PointCloud)
 
-    scaled_baseline = baseline / DEPTH_SCALE
+    pcd.project_to_rgbd_image
+
+    device = o3d.core.Device("CPU:0")
+    baseline = 120.0 / 1000.0
+    pcd.transform([[1, 0, 0, -baseline], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
     open3d_right_intrinsic = right_camera_intrinsics
 
     print(f"{open3d_right_intrinsic=}")
-    if axis == 0:
-        extrinsics = [[1, 0, 0, -scaled_baseline], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-    elif axis == 1:
-        extrinsics = [[1, 0, 0, 0], [0, 1, 0, scaled_baseline], [0, 0, 1, 0], [0, 0, 0, 1]]
-    elif axis == 2:
-        extrinsics = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, scaled_baseline], [0, 0, 0, 1]]
 
-    rgbd_reproj = pcd.project_to_rgbd_image(
-        shape[1], shape[0], intrinsic, extrinsics=extrinsics, depth_scale=DEPTH_SCALE, depth_max=DEPTH_MAX
-    )
+    rgbd_reproj = pcd.project_to_rgbd_image(shape[1], shape[0], intrinsic, depth_scale=1000.0, depth_max=10.0)
     color_legacy = np.asarray(rgbd_reproj.color.to_legacy())
     depth_legacy = np.asarray(rgbd_reproj.depth.to_legacy())
+
+    assert len(color_legacy.shape) == 3
+    assert color_legacy.shape[2] == 3
+    assert len(depth_legacy.shape) == 2
+    assert depth_legacy.shape == shape
+
+    print(f"{color_legacy.dtype=}")
+    print(f"{depth_legacy.dtype=}")
+    print(f"{np.max(depth_legacy.flatten())=}")
+    print(f"{np.max(color_legacy.flatten())=}")
+    print(f"{np.min(depth_legacy.flatten())=}")
+    print(f"{np.min(color_legacy.flatten())=}")
+    outdir = Path("reprojected_open3d")
     outdir.mkdir(exist_ok=True, parents=True)
-    depth_out = outdir / f"depth_{left_name.stem}.png"
-    color_out = outdir / f"color_{left_name.stem}.png"
+    depth_out = outdir / "depth.png"
+    color_out = outdir / "color.png"
 
     skimage.io.imsave(color_out, color_legacy)
     print(f"saved {color_out}")
     skimage.io.imsave(depth_out, depth_legacy)
     print(f"saved {depth_out}")
 
-
 if __name__ == "__main__":
-    from pathlib import Path
-
-    disparity = np.load("../test/test-imgs/disparity-IGEV/left_motorcycle.npy")
-    left_name = "../test/test-imgs/left/left_motorcycle.png"
-    left_image = skimage.io.imread(left_name)
-    outdir = Path("reprojected_open3d")
-    axis = 1
-    o3d_gen_right_image(disparity, left_image, outdir, left_name, axis)
+    test_o3d_reproject()
