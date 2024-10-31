a0ce60ab398c096cc1d5fd11127d2fdcfb6c6ac5
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-10-31 14:48:35 +0900

    changed API to o3d based and use skimage.io.imread() (#101)
    why
    公開APIを切り替えたい。
    what
    公開APIを切り替えたい。
    色順序問題の解決のためskimage.io.imread()に置換えた。

--- disparity_view/__init__.py
+++ disparity_view/__init__.py
@@ -1,8 +1,8 @@
 from .view import view_npy
 from .view import depth_overlay, as_colorimage
 from .depth_to_normal import DepthToNormalMap
-from .cv_reprojection import reproject_from_left_and_disparity, gen_right_image
-from .cv_reprojection import make_animation_gif
+from .o3d_reprojection import reproject_from_left_and_disparity, gen_right_image
+from .o3d_reprojection import make_animation_gif
 
 from .zed_info import (
     get_width_height_fx_fy_cx_cy,
