4fd754990cd4e8ea3a978ca54b17309a52828212
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-05 16:27:40 +0900

    project_to_rgbd_image(self, extrinsics=o3d.core.Tensor(np.eye(4, dtype=np.float32))):

--- disparity_view/projection_class.py
+++ disparity_view/projection_class.py
@@ -42,14 +42,13 @@ class StereoCamera:
         assert disparity_map.shape[:2] == left_image.shape[:2]
         return generate_point_cloud(disparity_map, left_image, self.left_camera_matrix, self.baseline)
 
-    def project_to_rgbd_image(self):
+    def project_to_rgbd_image(self, extrinsics=o3d.core.Tensor(np.eye(4, dtype=np.float32))):
         height, width = self.shape[:2]
         assert isinstance(width, int)
         assert isinstance(height, int)
         assert isinstance(self.left_camera_matrix, o3d.core.Tensor)
         assert isinstance(self.right_camera_matrix, o3d.core.Tensor)
-        extrinsics = o3d.core.Tensor(np.eye(4, dtype=np.float32))
-        assert isinstance(extrinsics, o3d.core.Tensor)
+        assert isinstance(extrinsics, o3d.core.Tensor) or isinstance(extrinsics, np.ndarray)
         return self.pcd.project_to_rgbd_image(
             width, height, intrinsics=self.left_camera_matrix, extrinsics=extrinsics, depth_scale=DEPTH_SCALE,
             depth_max=DEPTH_MAX
