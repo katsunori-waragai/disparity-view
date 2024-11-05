ffdb4ed67833fc4100e2cc3fb2d831e3955d57dc
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-05 17:21:52 +0900

    use command line argument

--- scripts/sample_class.py
+++ scripts/sample_class.py
@@ -6,23 +6,30 @@ from disparity_view.o3d_project import as_extrinsics
 from disparity_view.projection_class import StereoCamera
 from disparity_view.util import  safer_imsave
 
+import skimage.io
+
 if __name__ == "__main__":
-    import skimage.io
-    left_name = Path("../test/test-imgs/left/left_motorcycle.png")
-    disparity_name = Path("../test/test-imgs/disparity-IGEV/left_motorcycle.npy")
-    outdir = Path("out_class")
+    """
+    python3 project.py ../test/test-imgs/disparity-IGEV/left_motorcycle.npy ../test/test-imgs/left/left_motorcycle.png
+    """
+    import argparse
 
-    assert left_name.is_file()
-    assert left_name.lstat().st_size > 0
-    assert disparity_name.is_file()
-    assert disparity_name.lstat().st_size > 0
+    parser = argparse.ArgumentParser(description="reprojector")
+    parser.add_argument("disparity", help="disparity npy file")
+    parser.add_argument("left", help="left image file")
+    parser.add_argument("--axis", default=0, help="axis to shift(0: to right, 1: to upper, 2: to far)")
+    parser.add_argument("--gif", action="store_true", help="git animation")
+    parser.add_argument("--outdir", default="output", help="output folder")
+    args = parser.parse_args()
 
-    axis = 0
+    disparity_name = Path(args.disparity)
+    left_name = Path(args.left)
+    axis = int(args.axis)
+    outdir = Path(args.outdir)
+
     left_image = skimage.io.imread(str(left_name))
     disparity = np.load(str(disparity_name))
 
-    assert len(left_image.shape) == 3
-    assert len(disparity.shape) == 2
     height, width = disparity[:2]
 
     stereo_camera = StereoCamera()
@@ -34,7 +41,7 @@ if __name__ == "__main__":
     extrinsics = as_extrinsics(tvec)
     projected = stereo_camera.project_to_rgbd_image(extrinsics=extrinsics)
     color_legacy = np.asarray(projected.color.to_legacy())
-    outfile = Path("out_class") / "color_left_motorcycle.png"
+    outfile = outdir / "color_left_motorcycle.png"
     outfile.parent.mkdir(exist_ok=True, parents=True)
     safer_imsave(str(outfile), color_legacy)
     assert outfile.lstat().st_size > 0
