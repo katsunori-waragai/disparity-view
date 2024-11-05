6f076cea0c1039900f7c645ebd688b1ee9ae8818
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-05 16:28:36 +0900

    start working class version

--- scripts/sample_class.py
+++ scripts/sample_class.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import numpy as np
 
+from disparity_view.o3d_project import gen_tvec, DEPTH_SCALE
+from disparity_view.o3d_project import as_extrinsics
 from disparity_view.projection_class import StereoCamera
 from disparity_view.util import  safer_imsave
 
@@ -27,7 +29,10 @@ if __name__ == "__main__":
     shape = disparity.shape
     stereo_camera.set_camera_matrix(shape=shape, focal_length=1070)
     stereo_camera.pcd = stereo_camera.generate_point_cloud(disparity, left_image)
-    projected = stereo_camera.project_to_rgbd_image()
+    scaled_baseline = 120 / DEPTH_SCALE # [mm]
+    tvec=gen_tvec(scaled_shift=scaled_baseline, axis=0)
+    extrinsics = as_extrinsics(tvec)
+    projected = stereo_camera.project_to_rgbd_image(extrinsics=extrinsics)
     color_legacy = np.asarray(projected.color.to_legacy())
     outfile = Path("out_class") / "color_left_motorcycle.png"
     outfile.parent.mkdir(exist_ok=True, parents=True)
