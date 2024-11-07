8a7630d84d5c556e0951622f2ec9bb5605d95d86
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-07 16:40:35 +0900

    revised comment

--- scripts/project.py
+++ scripts/project.py
@@ -21,7 +21,7 @@ import disparity_view
 
 if __name__ == "__main__":
     """
-    python3 project.py ../test/test-imgs/disparity-IGEV/left_motorcycle.npy ../test/test-imgs/left/left_motorcycle.png
+    python3 project.py ../test/test-imgs/disparity-IGEV/left_motorcycle.npy ../test/test-imgs/left/left_motorcycle.png ../test/zed-imgs/camera_param.json
     """
     import argparse
 
