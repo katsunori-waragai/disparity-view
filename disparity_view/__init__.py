8bd183b2f5ea8258fe16d88b35b0f39acded4eda
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-07 15:32:57 +0900

    removed unused

--- disparity_view/__init__.py
+++ disparity_view/__init__.py
@@ -1,7 +1,7 @@
 from .view import view_npy
 from .view import depth_overlay, as_colorimage
 from .depth_to_normal import DepthToNormalMap
-from .o3d_project import project_from_left_and_disparity, gen_right_image
+from .o3d_project import gen_right_image
 from .o3d_project import make_animation_gif
 from .o3d_project import StereoCamera
 
