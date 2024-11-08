bb366eb4ae7acde0aef05292b1af2f2ac0dff0d3
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-08 16:45:46 +0900

    use import disparity_view

--- scripts/read_camera_json.py
+++ scripts/read_camera_json.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from disparity_view.cam_param import CameraParameter
+import disparity_view
 
 json_file = Path("../test/zed-imgs/camera_param.json")
-camera_parameter = CameraParameter.load_json(json_file)
+camera_parameter = disparity_view.CameraParameter.load_json(json_file)
 print(f"{camera_parameter=}")
 print(f"{camera_parameter.to_matrix()=}")
 print(f"{camera_parameter.get_baseline()=}")
