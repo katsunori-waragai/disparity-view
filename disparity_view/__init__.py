0e8bcf135b3265493c0fd79759347a09c0dcee68
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-08 16:50:59 +0900

    gen_tvec, as_extrinsics in __init__.py

--- disparity_view/__init__.py
+++ disparity_view/__init__.py
@@ -4,7 +4,8 @@ from .depth_to_normal import DepthToNormalMap
 from .o3d_project import gen_right_image
 from .o3d_project import make_animation_gif
 from .o3d_project import StereoCamera
-
+from .o3d_project import gen_tvec
+from .o3d_project import as_extrinsics
 from .cam_param import CameraParameter
 
 __version__ = "0.0.17"
