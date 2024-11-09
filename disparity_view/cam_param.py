a15259d4c0dfb69e1204aa1a3ee0c44ac729a64c
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-11-09 10:18:16 +0900

    revised doc comment (#138)
    what
    revised doc comment

--- disparity_view/cam_param.py
+++ disparity_view/cam_param.py
@@ -1,9 +1,5 @@
 """
-module to get zed2i camera by StereoLabs
-
-- Camera parameters are obtained by zed sdk.
-- Camera parameters are saved in /usr/local/zed/settings/SN*.conf
-    The file format is toml.
+module for Camera Parameter
 """
 
 from dataclasses import dataclass
@@ -29,17 +25,17 @@ class CameraParameter:
 
     width: int = 0  # [pixel]
     height: int = 0  # [pixel]
-    fx: float = 0.0
-    fy: float = 0.0
+    fx: float = 0.0  # [pixel]
+    fy: float = 0.0  # [pixel]
     cx: float = 0.0  # [pixel]
     cy: float = 0.0  # [pixel]
-    baseline: float = 0.0
+    baseline: float = 0.0  # [mm]
 
     def save_json(self, name: Path):
         open(name, "wt").write(self.to_json())
 
     @classmethod
-    def load_json(cls, name: Path):
+    def load_json(cls, name: Path):  # from_json() に名前を変える
         return cls.from_json(open(name, "rt").read())
 
     @classmethod
@@ -47,6 +43,12 @@ class CameraParameter:
         """
         Note:
             cam_info = zed.get_camera_information()
+
+        function to get zed2i camera by StereoLabs
+
+        - Camera parameters are obtained by zed sdk.
+        - Camera parameters are saved in /usr/local/zed/settings/SN*.conf
+            The file format is toml.
         """
         left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
         width, height, fx, fy, cx, cy = (
