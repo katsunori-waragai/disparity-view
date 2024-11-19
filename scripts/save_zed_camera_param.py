ddafbbef0232053afb8f9eca2957f5b6ca323ff3
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-11-19 19:39:49 +0900

    save camera parameter file in each resolution (#152)
    why
    I want to get a json file of camera parameters for each resolution combination.
    what
    Extended to save json files for each candidate resolution.
    For simplicity, not much function extraction is done.

--- scripts/save_zed_camera_param.py
+++ scripts/save_zed_camera_param.py
@@ -4,22 +4,6 @@ import pyzed.sl as sl
 
 import disparity_view
 
-
-def get_zed_camerainfo():
-    zed = sl.Camera()
-
-    init_params = sl.InitParameters()
-
-    status = zed.open(init_params)
-    if status != sl.ERROR_CODE.SUCCESS:
-        print(f"Error opening camera: {status}")
-        sys.exit(1)
-
-    cam_info = zed.get_camera_information()
-    zed.close()
-    return cam_info
-
-
 def get_width_height(cam_info):
     left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
     width = left_cam_params.image_size.width
@@ -34,10 +18,31 @@ if __name__ == "__main__":
     """
     from pathlib import Path
 
-    cam_info = get_zed_camerainfo()
-    camera_parameter = disparity_view.CameraParameter.create(cam_info)
-    width, height = get_width_height(cam_info)
-    outname = Path("out") / f"zed_{width}_{height}.json"
-    outname.parent.mkdir(exist_ok=True, parents=True)
-    camera_parameter.save_json(outname)
-    print(f"saved {outname}")
+    resolutions = [
+        sl.RESOLUTION.HD1080,
+        sl.RESOLUTION.HD1200,
+        sl.RESOLUTION.HD2K,
+        sl.RESOLUTION.HD720,
+        sl.RESOLUTION.SVGA,
+        sl.RESOLUTION.VGA,
+    ]
+
+    zed = sl.Camera()
+
+    init_params = sl.InitParameters()
+
+    for resolution in resolutions:
+        init_params.camera_resolution = resolution
+        status = zed.open(init_params)
+        if status != sl.ERROR_CODE.SUCCESS:
+            print(f"Error opening camera {resolution}: {status}")
+            continue
+
+        cam_info = zed.get_camera_information()
+        zed.close()
+        camera_parameter = disparity_view.CameraParameter.create(cam_info)
+        width, height = get_width_height(cam_info)
+        outname = Path("out") / f"zed_{width}_{height}.json"
+        outname.parent.mkdir(exist_ok=True, parents=True)
+        camera_parameter.save_json(outname)
+        print(f"saved {outname}")
