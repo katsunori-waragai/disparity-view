fd9a977e69ed1a4a2dd0307937d5e3ada7611ed2
katsunori-waragai <huyuhiko1128@gmail.com>
2026-05-31 14:42:24 +0900

    removed japanese comment

--- disparity_view/depth_to_normal.py
+++ disparity_view/depth_to_normal.py
@@ -1,6 +1,6 @@
 """
-簡易的な実装である。
-そのため、Open3dのライブラリで今後置き換える可能性あり。
+This is a simple implementation.
+It may be replaced by the Open3D library in the future.
 """
 
 from dataclasses import dataclass, field
@@ -24,10 +24,9 @@ class DepthToNormalMap:
         # Compute the normal vector for each pixel
         z_value = 0.1 * np.percentile(np.sqrt(dx**2 + dy**2), 50)  # median
         normal = np.dstack((-dx, -dy, np.full((rows, cols), z_value)))
-        # sqrt(dx**2 + dy**2 + z_value**2) を計算している
+
         norm = np.sqrt(np.sum(normal**2, axis=2, keepdims=True))
 
-        # それぞれの画素についてRGBのベクトルの大きさが１になるようにしている。
         normal = np.divide(normal, norm, out=np.zeros_like(normal), where=norm != 0)
 
         # Map the normal vectors to the [0, 255] range and convert to uint8
