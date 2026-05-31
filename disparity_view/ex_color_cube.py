e0d0f4138a17cf594b5d03eb288d988af27042ff
katsunori-waragai <huyuhiko1128@gmail.com>
2026-05-31 13:25:30 +0900

    working

--- disparity_view/ex_color_cube.py
+++ disparity_view/ex_color_cube.py
@@ -7,13 +7,13 @@ from color_cube import color_cube_mapping, depth_to_rgbcube
 
 
 def dummy_depth_image(npy_path : Path) -> np.ndarray:
+    """
+    Load a disparity image and convert it to depth.
+    """
     disparity = np.load(npy_path)
-
     depth = 1.0 / disparity
 
     depth *= 20
-    # depth = depth ** 2
-    # depth -= 0.1 * depth.min()
     return depth
 
 
@@ -36,6 +36,18 @@ def show_colormap(outdir: Path):
     plt.savefig(pngname)
 
 
+def modified_dummy_depth_image(depth: np.ndarray) -> np.ndarray:
+    """
+    元画像の範囲では、色が変化しにくいので、平均値で正規化してから、さらに非線形変換をかける。
+    """
+    mean_depth = np.mean(depth.flatten())
+    print(f"{mean_depth=}")
+    depth /= mean_depth
+    depth = depth**2.0
+    depth *= 30
+    return depth
+
+
 if __name__ == "__main__":
     repo_root = Path(__file__).resolve().parent.parent
     output_dir = repo_root / "output"
@@ -44,37 +56,26 @@ if __name__ == "__main__":
     npy_path =Path("../test/test-imgs/disparity-IGEV/left_motorcycle.npy")
     depth = dummy_depth_image(npy_path=npy_path)
 
-    def modified_dummy_depth_image(depth: np.ndarray) -> np.ndarray:
-        mean_depth = np.mean(depth.flatten())
-        print(f"{mean_depth=}")
-        depth /= mean_depth
-        depth = depth**2.0
-        depth *= 30
-        return depth
-
     depth = modified_dummy_depth_image(depth)
 
     print(f"{np.min(depth)=}")
     print(f"{np.max(depth)=}")
 
-    plt.figure()
-    plt.jet()
-    plt.imshow(depth)
-    plt.colorbar()
-    plt.show()
 
     rgb = depth_to_rgbcube(depth)
 
-    assert rgb.ndim==3
-    assert rgb.dtype in (np.float32, np.float64)
-    print(np.max(rgb.flatten()))
-
     plt.figure(figsize=(10,4))
+    plt.subplot(1,2,1)
+    plt.jet()
+    plt.imshow(depth)
+    plt.title("Depth -> RGB (jet mapping)")
+    plt.colorbar()
+
+    plt.subplot(1,2,2)
     plt.title("Depth -> RGB (Vision Banana style)")
     plt.imshow(rgb)
 
     plt.tight_layout()
-    plt.show()
     png_name = output_dir / "depth_to_rgbcube.png"
     plt.savefig(png_name)
     print(f"Saved: {png_name}")
